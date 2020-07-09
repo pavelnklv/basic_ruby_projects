7b68fda28bc8e9f6c528561011bfa421a6503cb9
pavelnklv <nikolaev_pavel@aol.com>
2020-07-09 19:28:22 +0300

    update fib.rb

--- fib.rb
+++ fib.rb
@@ -1,11 +1,33 @@
-def fib n
-  a, b = 0, 1
-  result = ''
-  while a < n
-    result += "#{a} "
-    a, b = b, a+b
+# def fib n
+#   a, b = 0, 1
+#   result = ''
+#   while a < n
+#     result += "#{a} "
+#     a, b = b, a+b
+#   end
+#   result
+# end
+
+# p fib 1000
+
+class Fib
+  def initialize n
+    @a, @b = 0, 1
+    @i = 0
+    @sum = 0    
+    @n = n
   end
-  result
+
+  def even_fibonacci
+    while @i <= @n
+      @i = @a + @b
+      @sum += @i if @i % 2 == 0
+      @a = @b
+      @b = @i
+    end
+    @sum
+  end
 end
 
-p fib 1000
+result = Fib.new 4_000_000_000_000
+p result.even_fibonacci
